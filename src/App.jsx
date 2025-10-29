fdfe84669dab08e64a337f0df34edfc4cf01fa16
Adolfo Tlatenchi <94709695+Adolf45@users.noreply.github.com>
2025-10-29 10:27:16 -0600

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,18 @@ import { Header } from "./UI/Header"
 function App() {
   return (
     <>
-      <Header/>
+       <div className="min-h-screen flex flex-col">
+         <Header/>
+      {/*
+          <Routes>
+          <Route path="/resetPasswordSuccess" element={<ResetPasswordSuccess />} />
+          <Route path="/resetPassword" element={<ResetPasswordPage />} />
+          <Route path="/loginPage" element={<LoginPage />} />
+          <Route path="/registroPage" element={<RegistroPage />} />
+        </Routes>
+      */}
+          <Footer />
+        </div> 
     </>
   )
 }
